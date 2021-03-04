85b97c63760510e31788e4f02e1e9346182b8a83
ThatGeekyWeeb <ThatGeekyWeeb@gmail.com>
2021-03-03 22:55:12 -0500

    Update code

--- miato.sh
+++ miato.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
-server=""
 miato="/home/mia/git/dcfs/mnt/$server/miato"
 while sleep 2; do
 cli() {
-       echo $(tac "$miato" | tail  -1 | cut -d',' -f3 | grep '^~M')
+       echo $(cat "$miato" | tail  -1 | cut -d',' -f3 | grep '^~M')
 }
-if $(tac "$miato" | tail  -1 | cut -d',' -f3 | grep -q '~M'); then
+if $(cat "$miato" | tail  -1 | cut -d',' -f3 | grep -q '~M'); then
 	case $(echo $(cli) | sed 's/ .*//g' | cut -d'M' -f2) in
 		color)
 			hex=$(echo $(cli) | cut -d' ' -f2)
 			if echo $(cli) | grep -q '@'; then
-				id=$( echo $(cli) | sed -e 's/.*<@!//g' -e 's/>//g')
+				id=$(echo $(cli) | sed -e 's/.*<@!//g' -e 's/>//g')
 			else
-				id=$(tac "$miato" | tail -1 | cut -d',' -f1)
+				id=$(cat "$miato" | tail -1 | cut -d',' -f1)
 			fi
 			echo "~role $id $hex" >> "$miato"
 			sleep 2
-			if [ $(tac "$miato" | tail -1 | cut -d' ' -f3) = "not" ]; then
+			if [ $(cat "$miato" | tail -1 | cut -d' ' -f3) = "not" ]; then
 				echo "~role create $hex $hex" >> "$miato"
 			sleep 2
 			echo "~role $id $hex" >> "$miato"
 			fi
-			sleep 2
-			echo "~purge all" >> "$miato"
 			;;
 		clean)
 			echo "~purge all" >> "$miato"
-			echo "Please do not chat here, the code breaks if there are 15 msgs/lines of msgs" >> "$miato"
+			echo "Please do not chat here" >> "$miato"
 			;;
 		quote)
-			fortune "/usr/share/fortunes/$(echo $(cli) | sed -e 's/.*<@!//g' -e 's/>//g')" >> "$miato" || {
+			[ -f "/usr/share/fortunes/$(echo $(cli) | sed -e 's/.*<@!//g' -e 's/>//g')" ] && {
+				fortune "/usr/share/fortunes/$(echo $(cli) | sed -e 's/.*<@!//g' -e 's/>//g')" >> "$miato"
+				echo "~$(echo $(cli) | sed -e 's/.*<@!//g -e s/>//g')" >> "$miato"
+			} || {
 				fortune >> "$miato"
 			}
 			;;
 		mkq)
-			id=$(tac "$miato" | tail -1 | cut -d',' -f1)
+			echo $(cli) | grep -q "<" && {
+				id=$(echo $(cli) | sed -e 's/.*<@!//g -e s/>//g')
+			} || {
+				id=$(cat "$miato" | tail -1 | grep '~Mmkq' |cut -d',' -f1)
+			}
 			echo $(echo $(cli) | sed 's/~Mmkq //g') >> "/usr/share/fortunes/$id"
 			echo '%' >> "/usr/share/fortunes/$id"
 			strfile "/usr/share/fortunes/$id"
@@ -44,27 +48,28 @@ if $(tac "$miato" | tail  -1 | cut -d',' -f3 | grep -q '~M'); then
 			#echo "~purge all" >> "$miato"
 			cat << 'EOF' >> "$miato"
 ```text
-Miato: A bot running on Mia's laptop
+Miato: A regular user, with a scripted client, running on Mia's laptop
 Usage: ~M<FUNC> [EX] - I.E: ~Mcolor <HEX>
-	Syntax: 
+	Syntax:
 		<>: Required
 		[]: Extra
 ***
 color: Create/Add/Remove a color role of HEX (~Mcolor <HEX> [@PING])
 clean: Purge the miato channel
 quote: Grab a quote from fortune or find a quote from @PING (~Mquote [@PING])
-mkq: Create a quote under your PING (~Mmkq <quote>)
+mkq: Create a quote under your PING (~Mmkq <quote>) # CURRENTLY BROKEN
 help: That prints this swilly uwu (~Mhelp)
 ***
-	~ Miato uses Carl-Bot as a text interface to role managment
+
+---
+Miato's scripting service is open source!
+Find it here!	> https://github.com/ThatGeekyWeeb/miato-dcfs/
+---
+
 ***
 ```
 EOF
 			;;
 	esac
-fi
-if [ $(cat "$miato" | grep ',*:' -c) -gt 15 ]; then
-	echo "~purge all" >> "$miato"
-	echo "Please do not chat here, the code breaks if there are 15 msgs/lines of msgs" >> "$miato"
 fi
 done
